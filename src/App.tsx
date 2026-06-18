0d72e7629d56b033469d4ff6627285ef5debc2ba
Brian Tarihoran <briyantarihoran@gmail.com>
2026-06-18 17:35:50 +0700

    Add fallback route to fix blank pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { ProjectDetail } from './pages/ProjectDetail';
 import { Header } from './components/Header';
@@ -16,6 +16,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:id" element={<ProjectDetail />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <VanillaAnimations />
     </Router>
